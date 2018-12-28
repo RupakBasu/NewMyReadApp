2e9513bff1ab1e41b71447b7ee702c50192b2102
RupakBasu <rupakbasu1@gmail.com>
2018-12-28 10:35:31 -0600

    Add files via upload
    searchpage.js has been updated still major problems

--- src/components/Page/Searchpage.js
+++ src/components/Page/Searchpage.js
@@ -5,21 +5,30 @@ import * as BooksAPI from "../../BooksAPI";
 import AddBookButton from "../BackToMainPage";
 import Book from "../Book";
 
+const api = "https://reactnd-books-api.udacity.com/books"
+
 class Searchpage extends React.Component {
-  componentDidMount() {
-    console.log(this);
-  }
+  // componentDidMount() {
+  //   console.log(this);
+  // }
 
   state = {
     booksInSearch: [],
     query: ""
   };
 
+  componentDidMount() {
+    fetch(api, { headers: { 'Authorization': 'This is Authorization for my books/:id' }})
+      // .then(response => response.json())
+      .then(response => response.text())
+      .then(text => console.log(text))
+      .then(books => this.setState({ booksInSearch: books.booksInSearch }));
+  }
+
   searchBarChange = e => {
     const query = e.target.value;
     this.setState({ [e.target.name]: query }, () => this.searchBooks(query));
   };
-
 
   searchBooks = query => {
     if (query === "") {
@@ -35,21 +44,30 @@ class Searchpage extends React.Component {
     }
   };
 
+  updateBookStates(searchBooks) {
+    return
+    searchBooks.map(book => BooksAPI.get(book.id));
+  }
+
   render() {
-    const {books}= this.props;
 
-    const bookList = this.props.books.map(foundBooks => {
-      if (this.props.books.id === foundBooks.id) {
-          foundBooks.shelf = this.props.books.shelf;
-      }
-
-      console.log(books.title)
-      console.log(books.id)
-      console.log(books.shelf)
-      console.log(foundBooks.title)
-      console.log(foundBooks.id)
-      console.log(foundBooks.shelf)
-    })
+    // const {books}= this.props;
+    //
+    // const bookList = this.props.books.map(foundBooks => {
+    //   if (this.props.books.id === foundBooks.id) {
+    //       foundBooks.shelf = this.props.books.shelf;
+    //     }
+    //   // } else {
+    //   //   foundBooks.shelf === 'none';
+    //   // }
+    //
+    //   console.log(books.title)
+    //   console.log(books.id)
+    //   console.log(books.shelf)
+    //   console.log(foundBooks.title)
+    //   console.log(foundBooks.id)
+    //   console.log(foundBooks.shelf)
+    // })
 
     return (
       <div className="search-books">
@@ -60,8 +78,14 @@ class Searchpage extends React.Component {
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
               You can find these search terms here:
               https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
+
+              {this.state.booksInSearch.length > 0 && this.state.booksInSearch.map(book => (
+                  <Book book={book} key={book.id} shelf={book.shelf}updateBookLocation={this.props.updateBookLocation}/> ))}
+
+                  {this.props.books.map(book => <Book book={book} key={book.id} updateBookLocation={this.props.updateBookLocation} /> )}
             */}
             <input
               type="text"
@@ -74,16 +98,13 @@ class Searchpage extends React.Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-
-            {this.state.booksInSearch.length > 0 &&
-              this.state.booksInSearch.map(book => (
-                <Book
-                  book={book}
-                  key={book.id}
-                  shelf={book.shelf}
-                  updateBookLocation={this.props.updateBookLocation}
-                />
-              ))}
+          {this.state.booksInSearch.length > 0 && this.state.booksInSearch.map(book => (
+              <Book book={book} key={book.id} shelf={book.shelf}updateBookLocation={this.props.updateBookLocation}/> ))}
+              {/*
+                The below code shows the books on myshelf with the correct shelf location on the dropdown but then I cannot get all my
+                books in the libaray to show up
+                {this.props.books.map(book => <Book book={book} key={book.id} updateBookLocation={this.props.updateBookLocation} /> )}
+              */}
           </ol>
         </div>
       </div>
